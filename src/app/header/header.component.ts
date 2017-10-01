27b77967168084b7b0cfc9e312900b23c000e73b
Mike Nauman <mike.s.nauman@rrd.com>
2017-10-01 13:50:40 -0500

    changed to rrd header so it passes linting.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'std-header',
+  selector: 'rrd-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.less']
 })
